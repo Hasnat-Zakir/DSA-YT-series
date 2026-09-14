d7f53296b09d27c61d8692f225ba5bed477ee738
Hasnat Zakir <hasnatzakir7258@gmail.com>
2026-09-14 11:08:03 -0700

    Added variables output examples

--- Variables and Data types/code.cpp
+++ Variables and Data types/code.cpp
@@ -2,8 +2,15 @@
 using namespace std;
 
 int main (){
-    cout <<"Hasnat Zakir";
-    return 0;
+cout << "Hello Hasnat\n";  
+int age = 20;
+char grade = 'A';
+float gpa = 3.5;
+bool isSafe = true;
+double pi = 3.14;
+
+cout << "age: " << age << "\ngrade: " << grade << "\ngpa: " << gpa << "\nisSafe: " << isSafe << "\npi: " << pi;
+  return 0;
 }
 
  
