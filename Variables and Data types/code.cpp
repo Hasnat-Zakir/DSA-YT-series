304c95b178dcbda4551b835c9a234c272ee11cb1
Hasnat Zakir <hasnatzakir7258@gmail.com>
2026-09-15 03:43:47 -0700

    git commit -m "Practice C++ output and variables"

--- Variables and Data types/code.cpp
+++ Variables and Data types/code.cpp
@@ -2,14 +2,72 @@
 using namespace std;
 
 int main (){
-cout << "Hello Hasnat\n";  
-int age = 20;
-char grade = 'A';
-float gpa = 3.5;
-bool isSafe = true;
-double pi = 3.14;
+// cout << "Hello Hasnat\n";  
+// int age = 20;
+// char grade = 'A';
+// float gpa = 3.5;
+// bool isSafe = true;
+// double pi = 3.14;
 
-cout << "age: " << age << "\ngrade: " << grade << "\ngpa: " << gpa << "\nisSafe: " << isSafe << "\npi: " << pi;
+// cout << "age: " << age << "\ngrade: " << grade << "\ngpa: " << gpa << "\nisSafe: " << isSafe << "\npi: " << pi;
+
+
+// Type Casting
+// char grade = 'A';
+// int value = grade;
+// cout << value << endl;
+
+// double price = 23.54;
+// int newPrice = (int)price;
+// cout << newPrice << endl;
+
+// Input in C++
+
+// int price;
+// cout << "Enter the price:";
+// cin >> price;
+// cout << "Price is: " << price << endl;
+
+// Operators in C++
+// int a = 10, b =  5;
+
+// cout << "sum = " << (a + b) << endl;
+// cout << "difference = " << (a - b) << endl;
+// cout << "product = " << (a * b) << endl;
+// cout << "division = " << (a / b) << endl;
+// cout << "modulus = " << (a % b) << endl;
+// cout << "reminder = " << (5 / (double)2) << endl;
+
+// Rational Operators
+// cout << (5 == 5) << endl;
+// cout << (5 != 5) << endl;
+// cout << (5 > 3) << endl;
+// cout << (5 < 3) << endl;
+// cout << (5 >= 5) << endl;
+// cout << (5 <= 5) << endl;
+
+// Logical Operators
+// cout << (5 == 5 && 3 < 5) << endl;
+// cout << (5 == 5 || 3 > 5) << endl;
+// cout << !(5 == 5) << endl;
+
+
+// Practice 
+
+// int a = 10 , b = 4;
+// int  sum = a + b ;
+// cout << " sum = " << sum << endl;
+
+int a, b;
+cout << "Enter number a ";
+cin >> a;
+
+cout << "Enter number b ";
+cin>> b;
+
+cout << "Sum = " << (a + b) <<endl;
+
+
   return 0;
 }
 
