aff2b3490091b33bb1f3c46e4df51493b848340a
Hasnat Zakir <hasnatzakir7258@gmail.com>
2026-09-16 12:40:35 -0700

    Added conditional statements and loops

---
+++ Conditional statements and Loops/code.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int n = -1;
+    // if (n >= 0){
+    //     cout << "Positive";
+    // }
+    // else {
+    //     cout << "Negative";
+    // }
+
+
+
+    // int age;
+    // cout << "Enter your age ";
+    // cin >> age;
+    // if (age >= 18){
+    //     cout << "You can vote";
+    // }
+    // else {
+    //     cout << "You can't vote";
+    // }
+
+    // int num;
+    // cout << "Enter the number ";
+    // cin >> num;
+
+    // if(num % 2 == 0){
+    //     cout << "Number is even";
+    // }
+    // else{
+    //     cout <<"Number is odd";
+    // }
+
+    // Find character is lower case or uppercase ;
+
+    // char ch;
+    // cout << "Enter Character : ";
+    // cin >> ch;
+    // if(ch >= 'a' && ch <= 'z'){
+    //      cout << "lowercase Character";
+    // }
+    // else{
+    //     cout << "UpperCase Character";
+    // }
+
+
+    // char ch;
+    // cout << "Enter Character : ";
+    // cin >> ch;
+    // if(ch >= 65 && ch <= 90){
+    //      cout << "UpperCase";
+    // }
+    // else{
+    //     cout << "LowerCase";
+    // }
+
+    // Ternay Statement
+
+    int n;
+    cout << "Enter number ";
+    cin >> n;
+
+   cout << ( n >= 0 ? "Positive" : "Negative");
+
+
+   return 0;
+} 
